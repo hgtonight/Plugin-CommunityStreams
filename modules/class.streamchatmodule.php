10932d63971e28799267b493af83370f920e10a1
Zachary Doll <Zachary.Doll@gmail.com>
2014-02-06 13:06:35 -0600

    Actually include the chat module

---
+++ modules/class.streamchatmodule.php
@@ -0,0 +1,36 @@
+<?php if (!defined('APPLICATION')) exit();
+/* Copyright 2014 Zachary Doll */
+
+class StreamChatModule extends Gdn_Module {
+   
+   protected $_Stream;
+   
+   public function __construct($Stream = FALSE, $Sender = '') {
+      $this->_Stream = $Stream;
+      parent::__construct($Sender);
+   }
+   
+   public function AssetTarget() {
+      return 'Panel';
+   }
+
+   public function ToString() {
+      if ($this->_Stream == FALSE) {
+         return '';
+      }
+      else {
+        switch($this->_Stream->Service) {
+          case 'twitch':
+            return '<iframe frameborder="0" scrolling="no" id="chat_embed" src="http://twitch.tv/chat/embed?channel='
+                    . $this->_Stream->AccountID
+                    . '&popout_chat=true" height="500" width="350"></iframe>';
+          case 'justin':
+            return '<iframe allowtransparency="true" frameborder="0" scrolling="no" id="chat_embed" src="http://www.justin.tv/chat/embed?channel='
+                    . $this->_Stream->AccountID
+                    . '&amp;default_chat=jtv&amp;#r=-rid-&amp;s=em"height="500" width="350"></iframe>';
+          default:
+            return '';
+        }
+      }
+   }
+}
